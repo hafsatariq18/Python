fe86a84e4762406731d8fe767bfd4745db4f4838
hafsatariq18 <163589125+hafsatariq18@users.noreply.github.com>
2024-03-18 21:54:39 +0530

    Create Runnerup_Score.py

---
+++ Runnerup_Score.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    winner =-120
+    runnerup =120
+    
+    for i in arr:
+        if i > winner:
+          runnerup = winner
+          winner = i
+        elif i<winner and i>runnerup:
+          runnerup = i
+    print(runnerup)
+        
