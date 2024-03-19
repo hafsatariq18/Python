82d5847ff77ac24d5b52fd4fcf39a3ecbc932394
hafsatariq18 <163589125+hafsatariq18@users.noreply.github.com>
2024-03-19 22:24:21 +0530

    Create subject_scores.py

---
+++ subject_scores.py
@@ -0,0 +1,8 @@
+scores={}
+x=int(input("Enter scores of Physics: "))
+scores.update({"Physics":x})
+y=int(input("Enter scores of Maths: "))
+scores.update({"Maths":y})
+z=int(input("Enter scores of Chemistry: "))
+scores.update({"Chemistry":z})
+print(scores)
