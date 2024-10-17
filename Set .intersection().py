aad2b5bc5ae6dc84f1b22973d8f0bc9e72333a9a
hafsatariq18 <163589125+hafsatariq18@users.noreply.github.com>
2024-10-17 13:03:37 +0530

    Add solution for common subscribers using set intersection

---
+++ Set .intersection().py
@@ -0,0 +1,22 @@
+# The students of District College have subscriptions to English and French newspapers. Some students have subscribed only to English, some have subscribed only to French, and some have subscribed to both newspapers.
+
+# You are given two sets of student roll numbers. One set has subscribed to the English newspaper, one set has subscribed to the French newspaper. Your task is to find the total number of students who have subscribed to both newspapers.
+
+# Input Format
+
+# The first line contains n , the number of students who have subscribed to the English newspaper.
+# The second line contains n space separated roll numbers of those students.
+# The third line contains b, the number of students who have subscribed to the French newspaper.
+# The fourth line contains b space separated roll numbers of those students.
+
+# Output Format
+
+# Output the total number of students who have subscriptions to both English and French newspapers.
+
+N = int(input())
+english = list(map(int, input().split()))
+
+M = int(input())
+french = list(map(int, input().split()))
+total_students = set(english).intersection(set(french ))
+print(len(total_students))
