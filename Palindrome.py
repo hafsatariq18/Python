2538bfea69f3e4ec9f2c02ddcfb3152ab0080549
hafsatariq18 <163589125+hafsatariq18@users.noreply.github.com>
2024-11-11 10:52:05 +0530

    Add string palindrome check with reverse comparison

--- Palindrome.py
+++ Palindrome.py
@@ -1,3 +1,5 @@
+# List Palindrome Check
+
 list1=[1,2,3,2,1]
 list2=list1.copy()
 list2.reverse()
@@ -5,3 +7,18 @@ if list1 == list2:
   print("The list is palindrome")
 else:
   print("The list is not palindrome")
+
+  
+
+# String Palindrome_check
+
+X = input()
+
+output = ""
+for i in X:
+  output += i
+output = X[::-1]
+if output == X:
+  print ("Palindrome")
+else:
+  print ("Not Palindrome")
