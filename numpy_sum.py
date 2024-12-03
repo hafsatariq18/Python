7157443eb5e38094f80cb416ba1335f9d232c409
hafsatariq18 <163589125+hafsatariq18@users.noreply.github.com>
2024-12-03 12:32:39 +0530

    Add sum and product calculation for 2-D array

---
+++ numpy_sum.py
@@ -0,0 +1,16 @@
+# You are given a 2-D array with dimensions N X M.
+# Your task is to perform the sum tool over axis 0 and then find the  
+# product of that result.
+
+import numpy
+
+N,M = map(int, input().split( ))
+array = numpy.array([list(map(int, input().split( )))for i in range(M)])
+
+sum = (numpy.sum(array, axis = 0))
+# print(sum)
+
+product = 1
+for i in sum:
+  product *= i
+print(product)
