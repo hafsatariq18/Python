d4f4d65515443a5b9ae7bf04c72097396160d9a8
hafsatariq18 <163589125+hafsatariq18@users.noreply.github.com>
2025-03-09 23:27:43 +0530

    Fix syntax error and improve If-Else.py script

--- If-Else.py
+++ If-Else.py
@@ -7,11 +7,11 @@
 # import math
 
 
-import os
-import random
-import re
-import sys
-
+# Removing unnecessary imports that are causing issues
+# import os
+# import random
+# import re
+# import sys
 
 
 if __name__ == '__main__':
@@ -23,4 +23,4 @@ if __name__ == '__main__':
     elif n%2==0 and n in range (6,21):
       print("Weird")
     elif n %2==0 and n>20:
-      print("Not Weird").
+      print("Not Weird")
